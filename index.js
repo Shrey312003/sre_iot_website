64b7d4e1738589181e3879f905d499cabb168ce1
Shrey <shreybti@gmail.com>
2024-11-06 12:21:57 +0530

    Changes

--- index.js
+++ index.js
@@ -17,31 +17,34 @@ mqttClient.on('connect',()=>{
 });
 
 app.get('/',(req,res) =>{
-    const interfaces = os.networkInterfaces();
+    // const interfaces = os.networkInterfaces();
 
-    for(const val in interfaces){
-        for(const iface of interfaces[val]){
-            if(iface.family == 'IPv4' && !iface.internal){
-                console.log(`Server IP: ${iface.address}`)
-            }
-        }
-    }
+    // for(const val in interfaces){
+    //     for(const iface of interfaces[val]){
+    //         if(iface.family == 'IPv4' && !iface.internal){
+    //             console.log(`Server IP: ${iface.address}`)
+    //         }
+    //     }
+    // }
+    res.send("hello");
 })
 
 app.post('/data',(req,res) => {
     const data = req.body;
 
-    mqttClient.publish('user_data',JSON.stringify(data),(err)=>{
-        if(err){
-            console.error("Failed to publish to mqtt",err);
-            res.status(500).send("Failed to publish to mqtt");
-        }
+    // mqttClient.publish('user_data',JSON.stringify(data),(err)=>{
+    //     if(err){
+    //         console.error("Failed to publish to mqtt",err);
+    //         res.status(500).send("Failed to publish to mqtt");
+    //     }
 
-        else{
-            console.log('DAta published ',data);
-            res.status(200).send("Done");
-        }
-    })
+    //     else{
+    //         console.log('DAta published ',data);
+    //         res.status(200).send("Done");
+    //     }
+    // })
+
+    res.send(data);
 })
 
 app.listen(port,()=>{
